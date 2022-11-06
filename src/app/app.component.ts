62a17df0ada8551b2f3a888018ba12ff5c24c37c
Ricardo Sánchez <rsanchez@psi-mobile.com>
2022-11-06 12:35:18 +0100

    :zap: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { StripeCardElementOptions, StripeElementsOptions } from '@stripe/stripe-js';
+import { StripeCardComponent, StripeService } from 'ngx-stripe';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <form
+      id="card-form"
+      novalidate
+      (ngSubmit)="stripePayment($event)"
+      [formGroup]="stripeForm"
+    >
+      <input
+        type="text"
+        class="form-control"
+        formControlName="name"
+        placeholder="Name on card"
+      />
+      <ngx-stripe-card
+        [options]="cardOptions"
+        [elementsOptions]="elementsOptions"
+        (error)="onError($event)"
+      ></ngx-stripe-card>
+      <button type="submit" id="payment-btn" class="btn btn-success">
+        Make Payment
+      </button>
+    </form>
+  `,
 })
 export class AppComponent {
-  title = 'ngx-stripe-issue-175';
+  @ViewChild(StripeCardComponent, { static: false }) card!: StripeCardComponent;
+
+  stripeForm!: FormGroup;
+
+  elementsOptions: StripeElementsOptions = {
+    locale: 'he',
+  };
+
+  cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        iconColor: '#888',
+        color: '#000',
+        fontWeight: '300',
+        fontFamily: '"Rubik", Helvetica, sans-serif',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#888',
+        },
+      },
+    },
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private stripeService: StripeService
+  ) {}
+
+  ngOnInit() {
+    this.stripeForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+    });
+  }
+
+  stripePayment(ev: Event) {
+    const { name } = this.stripeForm.value;
+
+    this.stripeService
+      .createToken(this.card.getCard(), { name })
+      .subscribe((result) => {
+        if (result.token) {
+          // do something
+          alert('Token ' + JSON.stringify(result.token));
+        }
+      });
+  }
+
+  onError(err: any) {
+    alert(err);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { NgxStripeModule } from 'ngx-stripe';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgxStripeModule.forRoot('pk_test_nDR7IWEIGLp4a1SBtqKH5eyg')
   ],
   providers: [],
   bootstrap: [AppComponent]
